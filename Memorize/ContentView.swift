b8b18dd0d901b18a5059ee068c262d97dc94f47f
Omar ElDessouki <omareldessouki@gmail.com>
2025-03-22 01:21:36 -0400

    -Added functionality to theme buttons to create shuffled cards

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,24 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let emojisFood: Array<String> = ["🍎", "🍎", "🍊", "🍊", "🍇", "🍇", "🍒", "🍒"]
+    let emojisParty: Array<String> = ["🎉", "🎉", "🎊", "🎊", "🥳", "🥳", "🪅", "🪅", "🎁", "🎁"]
+    let emojisPets: Array<String> = ["🐶", "🐶", "🐱", "🐱", "🐰", "🐰", "🐹", "🐹", "🐕", "🐕", "🐩", "🐩"]
+    
+    @State var emojis: Array<String> = []
+
+    let columns = Array(repeating: GridItem(.flexible()),count:5)
+    
     var body: some View {
-        let emojisFood: Array<String> = ["🍎", "🍊", "🍇", "🍒"]
-        let emojisParty: Array<String> = ["🎉", "🎊", "🥳", "🪅"]
-        let emojisPets: Array<String> = ["🐶", "🐱", "🐰", "🐹"]
-        let themes: Array<Array> = [emojisFood, emojisParty, emojisPets]
-       
+
         VStack{
             Text("Memorize!").font(.largeTitle)
-            HStack {
-                ForEach(0..<emojisFood.count, id: \.self){ index in
-                    CardView(content: emojisFood[index])
+            Spacer()
+            LazyVGrid(columns:columns, spacing: 61) {
+                        ForEach(0..<emojis.count, id: \.self){ index in
+                            CardView(content: emojis[index])
+                        }
+                    .foregroundColor(.teal)
                 }
-            }
-            .foregroundColor(.teal)
-            
+            Spacer()
             HStack{
                 Button(action: {
-                    
+                    emojis = emojisFood.shuffled()
                 }){
                     VStack {
                         Image(systemName: "carrot.fill")
@@ -34,7 +40,7 @@ struct ContentView: View {
                 }
                 Spacer()
                 Button(action: {
-                    
+                    emojis = emojisParty.shuffled()
                 }){
                     VStack {
                         Image(systemName: "party.popper.fill")
@@ -43,7 +49,7 @@ struct ContentView: View {
                 }
                 Spacer()
                 Button(action: {
-                    
+                    emojis = emojisPets.shuffled()
                 }){
                     VStack {
                         Image(systemName: "pawprint.circle.fill")
@@ -60,7 +66,7 @@ struct ContentView: View {
 
 struct CardView: View{
     let content: String
-    @State var isFaceUp = false
+    @State var isFaceUp = true
     
     var body: some View{
         ZStack{
